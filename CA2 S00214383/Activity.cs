55895032963a3a6cf240fd239da9f9533ab0eae8
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2021-12-06 11:30:23 +0000

    Added some filter, fixed a problem on the add and remove button, DateTime string to short version

--- CA2 S00214383/Activity.cs
+++ CA2 S00214383/Activity.cs
@@ -8,40 +8,53 @@ using System.Collections;
 
 namespace CA2_S00214383
 {
-    class Activity : IComparable
+    //enum to get activity type
+    public enum ActivityType { Air, Water, Land }
+
+    public class Activity : IComparable
     {
         public string Name { get; set; }
 
-        public DateTime dateTime { get; set; }
+
+        
+        public DateTime ActivityDate { get; set; }
 
         public decimal Cost { get; set; }
 
-        private string Description;
-
-        public string MyProperty
+        //object to display activities details plus cost
+        public string Description
         {
             get { return Description + Cost; }
             set { Description = value; }
         }
 
+        public ActivityType TypeOfActivity { get; set; }
 
-        //enum to get activity type
-        public enum ActivityType { Air, Water, Land }
 
-        public class TypeOfActivity
+        //public class TypeOfActivity
+        //{
+        //    private TypeOfActivity activity;
+
+        //    public void SetActivity(TypeOfActivity value)
+        //    {
+        //        activity = value;
+        //    }
+
+        //    public TypeOfActivity GetActivity()
+        //    {
+        //        return activity;
+        //    }
+        //}
+
+        //method to output details and cost for each activity
+        public string GetActivitiesDetails()
         {
-            private TypeOfActivity activity;
-
-            public void SetActivity(TypeOfActivity value)
-            {
-                activity = value;
-            }
-
-            public TypeOfActivity GetActivity()
-            {
-                return activity;
-            }
+            return $"Activity: {Name} \nCost per person: {Cost:c}";
         }
+
+
+
+
 
         public int CompareTo(object obj)
         {
@@ -51,7 +64,7 @@ namespace CA2_S00214383
             Activity activitySelected = obj as Activity;
             if (activitySelected == null)
 
-                return this.dateTime.CompareTo(activitySelected.dateTime);
+                return this.ActivityDate.CompareTo(activitySelected.ActivityDate);
             else
                 throw new NotImplementedException();
         }
@@ -59,7 +72,7 @@ namespace CA2_S00214383
 
         public override string ToString()
         {
-            return $"{Name }, {dateTime}";
+            return $"{Name }, {ActivityDate.ToShortDateString()}";
         }
 
 
--- CA2 S00214383/MainWindow.xaml.cs
+++ CA2 S00214383/MainWindow.xaml.cs
@@ -12,15 +12,23 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
+using System.Collections.ObjectModel;
 
 namespace CA2_S00214383
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
-        List<Activity> atividades;
+        // List<Activity> atividades;
+        ObservableCollection<Activity> atividades;
+        ObservableCollection<Activity> filteredActivities = new ObservableCollection<Activity>();
+        ObservableCollection<Activity> selectedActivities = new ObservableCollection<Activity>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,22 +36,21 @@ namespace CA2_S00214383
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //create activities
-            Activity trecking = new Activity() { Name = "Trecking", dateTime = DateTime.Now };
-            Activity Kayaking = new Activity() { Name = "Kayaking", dateTime = DateTime.Now };
-            Activity Parachuting = new Activity() { Name = "Parachuting", dateTime = DateTime.Now };
-            Activity MountainBike = new Activity() { Name = "Mountain Bike", dateTime = DateTime.Now };
-            Activity Surfing = new Activity() { Name = "Surfing", dateTime = DateTime.Now };
-            Activity HangGliding = new Activity() { Name = "Hang Gliding", dateTime = DateTime.Now };
-            Activity Abseling = new Activity() { Name = "Abseling", dateTime = DateTime.Now };
-            Activity Sailing = new Activity() { Name = "Sailing", dateTime = DateTime.Now };
-            Activity Helicopter = new Activity() { Name = "Helicopter Tour", dateTime = DateTime.Now };
+            //create activities objects and properties
+            Activity trecking = new Activity() { Name = "Trekking", ActivityDate = new DateTime(2021, 6, 01), Cost = 40.90m, TypeOfActivity = ActivityType.Land};
+            Activity Kayaking = new Activity() { Name = "Kayaking", ActivityDate = new DateTime(2021, 6, 01), Cost = 69.90m, TypeOfActivity = ActivityType.Water };
+            Activity Parachuting = new Activity() { Name = "Parachuting", ActivityDate = new DateTime(2021, 6, 01), Cost = 149.90m, TypeOfActivity = ActivityType.Air };
+            Activity MountainBike = new Activity() { Name = "Mountain Bike", ActivityDate = new DateTime(2021, 6, 02), Cost = 99.90m, TypeOfActivity = ActivityType.Land};
+            Activity Surfing = new Activity() { Name = "Surfing", ActivityDate = new DateTime(2021, 6, 02), Cost = 59.90m, TypeOfActivity = ActivityType.Water };
+            Activity HangGliding = new Activity() { Name = "Hang Gliding", ActivityDate = new DateTime(2021, 6, 02), Cost = 149.90m, TypeOfActivity = ActivityType.Land };
+            Activity Abseling = new Activity() { Name = "Abseling", ActivityDate = new DateTime(2021, 6, 03), Cost = 79.90m, TypeOfActivity = ActivityType.Air };
+            Activity Sailing = new Activity() { Name = "Sailing", ActivityDate = new DateTime(2021, 6, 03), Cost = 219.90m, TypeOfActivity = ActivityType.Water };
+            Activity Helicopter = new Activity() { Name = "Helicopter Tour", ActivityDate = new DateTime(2021, 6, 03), Cost = 199.90m, TypeOfActivity = ActivityType.Air };
 
+            //creating a collection
+            atividades = new ObservableCollection<Activity>();
 
-            //add to list
-            atividades = new List<Activity>();
-
-            // display on screen
+            // adding the objects to the collection, display on screen
             atividades.Add(trecking);
             atividades.Add(Kayaking);
             atividades.Add(Parachuting);
@@ -53,14 +60,112 @@ namespace CA2_S00214383
             atividades.Add(Abseling);
             atividades.Add(Sailing);
             atividades.Add(Helicopter);
-            
+
             //naming object property to output
-            listBoxActivities.ItemsSource = atividades;
+            listBoxActivities.ItemsSource = atividades;   
 
         }
+
+        //this method checks the activity selected and add details
+        private void listBoxActivities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //determine what was selected
+            Activity selectedActivity = listBoxActivities.SelectedItem as Activity;
+
+            //null check
+            if (selectedActivity != null)
+            {
+                //update display
+                tblkActivitiesDetails.Text = selectedActivity.GetActivitiesDetails();
+            }
+            //take action - update display
+        }
+
+        private void btnAddActivity_Click(object sender, RoutedEventArgs e)
+        {
+            Activity selected = listBoxActivities.SelectedItem as Activity;
+
+            //check not null
+            if (selected != null)
+            {
+                //remove from one list
+                atividades.Remove(selected);
+
+                //add to the other
+                lbxActSelected.Items.Add(selected);
+            }
+        }
+
+        //method to remove from second list and add to the first one
+        private void btnRemoveActivity_Click(object sender, RoutedEventArgs e)
+        {
+            //see what is selected
+            Activity selected2 = lbxActSelected.SelectedItem as Activity;
+
+            //check not null
+            if (selected2 != null)
+            {
+                //remove from one list
+                lbxActSelected.Items.Remove(selected2);
+
+                //add to the other (first list)
+                atividades.Add(selected2);
+            }
+        }
+
+        //applying filter by TypeOfActivity property
+        private void rb_Checked(object sender, RoutedEventArgs e)
+        {
+            //reset the filter
+            filteredActivities.Clear();
+
+            //if all checked, display all
+            if(rbAll.IsChecked == true)
+            {
+                listBoxActivities.ItemsSource = atividades;
+            }
+
+            //else if Land is selected display Land
+            if (rbLand.IsChecked == true)
+            {
+                foreach (Activity atividade in atividades)
+                {
+                    if (atividade.TypeOfActivity == ActivityType.Land)
+                    {
+                        filteredActivities.Add(atividade);
+                    }
+                }
+
+                listBoxActivities.ItemsSource = filteredActivities;
+            }
+
+            //else if Water is selected display Water
+            if (rbWater.IsChecked == true)
+            {
+                foreach (Activity atividade in atividades)
+                {
+                    if (atividade.TypeOfActivity == ActivityType.Water)
+                    {
+                        filteredActivities.Add(atividade);
+                    }
+                }
+
+                listBoxActivities.ItemsSource = filteredActivities;
+            }
+
+            //else if Air is selected display Air
+            if (rbAir.IsChecked == true)
+            {
+                foreach (Activity atividade in atividades)
+                {
+                    if (atividade.TypeOfActivity == ActivityType.Air)
+                    {
+                        filteredActivities.Add(atividade);
+                    }
+                }
+
+                listBoxActivities.ItemsSource = filteredActivities;
+            }
+        }
     }
-
-
-    
-   
-}
+    }
